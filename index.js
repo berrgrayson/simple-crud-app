1c3289c5ad51e007eb44551a4b19b7af573ad053
berrgrayson <niyongaboberrygray@gmail.com>
2024-10-23 13:50:00 +0200

    Adding backend API

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated!");
+});
+
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:p%40ssw0rd@backenddb.oecmt.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to the database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
